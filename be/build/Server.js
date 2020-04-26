c5ac2381dc48d4068c5cf516f84491d5aba5b1c0
Juan Andrés Mezzera <jmezzera@arnaldocastro.com.uy>
2020-04-26 14:49:01 -0300

    compilacion

---
+++ be/build/Server.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Server = /** @class */ (function () {
+    function Server() {
+    }
+    return Server;
+}());
+exports.default = Server;
+//# sourceMappingURL=Server.js.map
---
+++ be/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Server_1 = require("./Server");
+var server = new Server_1.default();
+console.log("Starting server");
+//# sourceMappingURL=index.js.map
