6c11fa7e2a36298f7c12b433928fd4abbde3d1dd
Juan Andrés Mezzera <jmezzera@arnaldocastro.com.uy>
2020-04-26 15:53:45 -0300

    Instancio el servidor web

--- be/build/index.js
+++ be/build/index.js
@@ -3,4 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Server_1 = require("./Server");
 var server = new Server_1.default();
 console.log("Starting server");
+server.run();
 //# sourceMappingURL=index.js.map
--- be/src/Server.ts
+++ be/src/Server.ts
@@ -1,3 +1,12 @@
+import WebServer from "./Infra/Web/WebServer";
+import ExpressWebServer from "./Infra/Web/ExpressWebServer";
+
 export default class Server {
-    constructor() {}
+    private webServer: WebServer;
+    constructor() {
+        this.webServer = new ExpressWebServer();
+    }
+    public run() {
+        this.webServer.listen(8080);
+    }
 }
