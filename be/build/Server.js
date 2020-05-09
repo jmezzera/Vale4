97243a4004d45c40e5346e7b3cc375b349a6b735
Juan Andrés Mezzera <jmezzera@arnaldocastro.com.uy>
2020-05-09 15:36:41 -0300

    instancio controladores

--- be/build/Server.js
+++ be/build/Server.js
@@ -1,9 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var ExpressWebServer_1 = require("./Infra/Web/ExpressWebServer");
+var TablesControllerImpl_1 = require("./UsesCases/TablesControllerImpl");
+var TablesDBDummy_1 = require("./Infra/DB/TablesDBDummy");
+var SocketHandler_1 = require("./Infra/Web/SocketHandler");
 var Server = /** @class */ (function () {
     function Server() {
-        this.webServer = new ExpressWebServer_1.default();
+        this.tablesDB = new TablesDBDummy_1.default();
+        this.tablesController = new TablesControllerImpl_1.default(this.tablesDB);
+        this.webServer = new ExpressWebServer_1.default({
+            tablesController: this.tablesController,
+        });
+        this.tablesSessionController = new SocketHandler_1.default(this.webServer.server, this.tablesController);
+        this.tablesController.tablesSessionController = this.tablesSessionController;
     }
     Server.prototype.run = function () {
         this.webServer.listen(8080);
--- be/src/Server.ts
+++ be/src/Server.ts
@@ -1,10 +1,27 @@
 import WebServer from "./Infra/Web/WebServer";
 import ExpressWebServer from "./Infra/Web/ExpressWebServer";
-
+import TablesController from "./UsesCases/TablesController";
+import TablesControllerImpl from "./UsesCases/TablesControllerImpl";
+import TablesDBDummy from "./Infra/DB/TablesDBDummy";
+import TablesDB from "./Infra/DB/TablesDB";
+import TablesSessions from "./Infra/Web/TablesSessions";
+import SocketHandler from "./Infra/Web/SocketHandler";
 export default class Server {
     private webServer: WebServer;
+    private tablesController: TablesController;
+    private tablesSessionController: TablesSessions;
+    private tablesDB: TablesDB;
     constructor() {
-        this.webServer = new ExpressWebServer();
+        this.tablesDB = new TablesDBDummy();
+        this.tablesController = new TablesControllerImpl(this.tablesDB);
+        this.webServer = new ExpressWebServer({
+            tablesController: this.tablesController,
+        });
+        this.tablesSessionController = new SocketHandler(
+            this.webServer.server,
+            this.tablesController
+        );
+        this.tablesController.tablesSessionController = this.tablesSessionController;
     }
     public run() {
         this.webServer.listen(8080);
