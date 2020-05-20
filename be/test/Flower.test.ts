25d1423b5385f7de56c5587428a0f9f18119c21a
Juan Andrés Mezzera <jmezzera@arnaldocastro.com.uy>
2020-05-19 22:42:06 -0300

    Desarrollo test para probar los distintos casos

---
+++ be/test/Flower.test.ts
@@ -0,0 +1,117 @@
+import { expect } from "chai";
+import Card, { Suit } from "../src/Entities/Card";
+import Game from "../src/UseCases/GameUtils";
+
+describe("Flores", () => {
+    let game: Game;
+    before(() => {
+        game = new Game();
+    });
+    describe("Es flor", () => {
+        it("Tres del mismo palo, muestra de otro palo", () => {
+            const cards = [
+                new Card(Suit.Basto, 1),
+                new Card(Suit.Basto, 4),
+                new Card(Suit.Basto, 7),
+            ];
+            const sampleCard = new Card(Suit.Oro, 4);
+            expect(game.validateFlower(cards, sampleCard)).to.be.true;
+
+            const cards2 = [
+                new Card(Suit.Copa, 10),
+                new Card(Suit.Copa, 7),
+                new Card(Suit.Copa, 1),
+            ];
+            const sampleCard2 = new Card(Suit.Espada, 7);
+            expect(game.validateFlower(cards2, sampleCard2)).to.be.true;
+        });
+        it("Tres del mismo palo, muestra del mismo palo", () => {
+            const cards = [
+                new Card(Suit.Basto, 1),
+                new Card(Suit.Basto, 4),
+                new Card(Suit.Basto, 7),
+            ];
+            const sampleCard = new Card(Suit.Basto, 5);
+            expect(game.validateFlower(cards, sampleCard)).to.be.true;
+
+            const cards2 = [
+                new Card(Suit.Copa, 10),
+                new Card(Suit.Copa, 7),
+                new Card(Suit.Copa, 1),
+            ];
+            const sampleCard2 = new Card(Suit.Copa, 12);
+            expect(game.validateFlower(cards2, sampleCard2)).to.be.true;
+        });
+        it("Tres piezas", () => {
+            const cards = [
+                new Card(Suit.Basto, 2),
+                new Card(Suit.Basto, 4),
+                new Card(Suit.Basto, 5),
+            ];
+            const sampleCard = new Card(Suit.Basto, 10);
+            expect(game.validateFlower(cards, sampleCard)).to.be.true;
+
+            const cards2 = [
+                new Card(Suit.Copa, 10),
+                new Card(Suit.Copa, 11),
+                new Card(Suit.Copa, 12),
+            ];
+            const sampleCard2 = new Card(Suit.Copa, 4);
+            expect(game.validateFlower(cards2, sampleCard2)).to.be.true;
+        });
+        it("2 piezas", () => {
+            const cards = [
+                new Card(Suit.Basto, 2),
+                new Card(Suit.Basto, 4),
+                new Card(Suit.Espada, 5),
+            ];
+            const sampleCard = new Card(Suit.Basto, 10);
+            expect(game.validateFlower(cards, sampleCard)).to.be.true;
+
+            const cards2 = [
+                new Card(Suit.Copa, 10),
+                new Card(Suit.Espada, 7),
+                new Card(Suit.Copa, 12),
+            ];
+            const sampleCard2 = new Card(Suit.Copa, 4);
+            expect(game.validateFlower(cards2, sampleCard2)).to.be.true;
+        });
+        it("1 pieza", () => {
+            const cards = [
+                new Card(Suit.Basto, 2),
+                new Card(Suit.Basto, 4),
+                new Card(Suit.Espada, 5),
+            ];
+            const sampleCard = new Card(Suit.Espada, 10);
+            expect(game.validateFlower(cards, sampleCard)).to.be.true;
+
+            const cards2 = [
+                new Card(Suit.Espada, 10),
+                new Card(Suit.Espada, 7),
+                new Card(Suit.Copa, 12),
+            ];
+            const sampleCard2 = new Card(Suit.Copa, 4);
+            expect(game.validateFlower(cards2, sampleCard2)).to.be.true;
+        });
+    });
+    describe("No es flor", () => {
+        it("3 de distinto palo, ninguna pieza", () => {
+            const cards2 = [
+                new Card(Suit.Espada, 10),
+                new Card(Suit.Copa, 7),
+                new Card(Suit.Basto, 12),
+            ];
+            const sampleCard2 = new Card(Suit.Oro, 4);
+            expect(game.validateFlower(cards2, sampleCard2)).to.be.false;
+        });
+        it("3 de distinto palo, 1 pieza", () => {
+            const cards2 = [
+                new Card(Suit.Espada, 10),
+                new Card(Suit.Copa, 7),
+                new Card(Suit.Basto, 12),
+            ];
+            const sampleCard2 = new Card(Suit.Espada, 4);
+            expect(game.validateFlower(cards2, sampleCard2)).to.be.false;
+        });
+    });
+});
