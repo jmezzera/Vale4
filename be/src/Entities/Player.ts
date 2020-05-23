c88ddc5200fd1dd072f3570719ae8231a5e82377
Juan Andrés Mezzera <jmezzera@arnaldocastro.com.uy>
2020-05-23 14:32:26 -0300

    Entidad player

---
+++ be/src/Entities/Player.ts
@@ -0,0 +1,25 @@
+import User from "./User";
+import Card from "./Card";
+
+export default class Player extends User {
+    private _cards: Card[];
+    constructor(user: User) {
+        super(
+            user.nickname,
+            user.email,
+            user.password,
+            user.confirmPassword,
+            user.name,
+            user.surname
+        );
+        this._cards = [];
+    }
+
+    public dealCards(cards: Card[]): void {
+        this._cards = cards;
+    }
+
+    public get cards(): Card[] {
+        return this._cards;
+    }
+}
