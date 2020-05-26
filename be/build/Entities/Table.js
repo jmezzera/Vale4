8b1a46c4d6a1ac58f7150855290ebfbed689beb7
Juan Andrés Mezzera <jmezzera@arnaldocastro.com.uy>
2020-05-26 19:40:30 -0300

    Cambio estado a DEALING cuando se conecta el ultimo jugador

--- be/build/Entities/Table.js
+++ be/build/Entities/Table.js
@@ -6,6 +6,9 @@ var Table = /** @class */ (function () {
     function Table(name, playersQty, isProtected, password) {
         if (playersQty === void 0) { playersQty = 2; }
         if (isProtected === void 0) { isProtected = false; }
+        if (playersQty !== 2 && playersQty !== 4 && playersQty !== 6) {
+            throw new Error("Invalid players quantity");
+        }
         this._name = name;
         this._playersQty = playersQty;
         this._isProtected = isProtected;
@@ -28,7 +31,10 @@ var Table = /** @class */ (function () {
             throw new Error(); //FIXME: manejar caso borde
         }
         this._awaitingPlayers = this._awaitingPlayers.filter(function (player) { return player.nickname !== nickname; }); //Sacarlo de la lista de awaiting players
-        this._players.push(new Player_1.default(player));
+        var connectedPlayers = this._players.push(new Player_1.default(player));
+        if (connectedPlayers === this._playersQty) {
+            this._state = TableSate.DEALING;
+        }
     };
     Object.defineProperty(Table.prototype, "id", {
         /**
@@ -78,6 +84,23 @@ var Table = /** @class */ (function () {
         get: function () {
             return this._state;
         },
+        set: function (state) {
+            this._state = state;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Table.prototype, "playersQty", {
+        get: function () {
+            return this._playersQty;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Table.prototype, "players", {
+        get: function () {
+            return this._players;
+        },
         enumerable: true,
         configurable: true
     });
@@ -89,6 +112,7 @@ var TableSate;
     TableSate[TableSate["AWAITING_PLAYER"] = 0] = "AWAITING_PLAYER";
     TableSate[TableSate["AWAITING_CARD"] = 1] = "AWAITING_CARD";
     TableSate[TableSate["AWAITING_RESPONSE"] = 2] = "AWAITING_RESPONSE";
+    TableSate[TableSate["DEALING"] = 3] = "DEALING";
 })(TableSate || (TableSate = {}));
 exports.TableSate = TableSate;
 //# sourceMappingURL=Table.js.map
